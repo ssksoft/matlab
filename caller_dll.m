9fe38cf0a9877e73cd695814a68a1a056bb71de2
ssk <sasat.s.93@gmail.com>
2020-04-05 11:02:43 +0900

    MATLABからグローバル変数を入出力とするdll呼び出し成功。 .m->wrapper->gain.dll

--- caller_dll.m
+++ caller_dll.m
@@ -1,5 +1,11 @@
-loadlibrary('gain.dll','gain.h');
-calllib('gain','init')
-calllib('gain','gain')
-% 
-unloadlibrary gain
+if ~libisloaded('wrapper_matlab')
+    loadlibrary('wrapper_matlab.dll');
+end
+
+libfunctionsview('wrapper_matlab')
+x = libpointer('int32Ptr',1);
+y = libpointer('int32Ptr',0);
+
+[res,st,st2] = calllib('wrapper_matlab','wrapper_matlab',x,y)
+%
+unloadlibrary wrapper_matlab
---
+++ wrapper_matlab.h
@@ -0,0 +1,1 @@
+int wrapper_matlab(int *x, int *y);
