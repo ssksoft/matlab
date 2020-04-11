bc4184adb67b44c6cf8a43f615c6395ad188ae44
ssk <sasat.s.93@gmail.com>
2020-04-11 17:11:36 +0900

    [rev] run_all_test.m 2つのテストモデルについて、自動テスト実行成功。 次は、テストとして実行するslxファイル名をcsvファイルで設定可能にする。

--- run_all_test_signalbuilder/run_all_test.m
+++ run_all_test_signalbuilder/run_all_test.m
@@ -1,29 +1,36 @@
 clear all;
 
-%% テストモデルのファイルパスを取得
-slx_path = 'TEST_sample.slx';
+%% 全テストモデルのファイルパスを取得
+slx_path_all = {'TEST_sample.slx','TEST_sample2_NG.slx'}
 
-%% テストモデルの読み込み
-load_system(slx_path)
-
-%% 全ブロックのpathを抽出
-all_block_path = find_system()
-
-%% 'Builder'を含むブロックpathのindexを取得
-str_search_result = strfind(all_block_path, 'Builder');
-empty_cell = cellfun(@isempty, str_search_result);
-index_signal_builder = find(~empty_cell);
-
-%% Signal Builderのpathを取得
-path_signal_builder = all_block_path{index_signal_builder}
-
-%% Signal BuilderのGroup数を取得
-[time,data,signames,groupnames] = signalbuilder(path_signal_builder);
-num_group = length(groupnames);
-
-%% Group1からGroup nまでシミュレーション実行
-
-for i=1:1:num_group
-    signalbuilder(path_signal_builder, 'activegroup',i);
-    sim(slx_path);
+%% i個目のテストモデルを実行
+num_slx = length(slx_path_all);
+for i=1:1:num_slx
+    slx_path = slx_path_all{i};
+    
+    %% テストモデルの読み込み
+    load_system(slx_path)
+    
+    %% 全ブロックのpathを抽出
+    all_block_path = find_system()
+    
+    %% 'Builder'を含むブロックpathのindexを取得
+    str_search_result = strfind(all_block_path, 'Builder');
+    empty_cell = cellfun(@isempty, str_search_result);
+    index_signal_builder = find(~empty_cell);
+    
+    %% Signal Builderのpathを取得
+    path_signal_builder = all_block_path{index_signal_builder}
+    
+    %% Signal BuilderのGroup数を取得
+    [time,data,signames,groupnames] = signalbuilder(path_signal_builder);
+    num_group = length(groupnames);
+    
+    %% Group1からGroup num_groupまでシミュレーション実行
+    
+    for j=1:1:num_group
+        signalbuilder(path_signal_builder, 'activegroup',j);
+        sim(slx_path);
+    end
+    
 end
