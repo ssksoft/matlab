d423e88be7991ec38458e653f150014ad4896769
ssk <sasat.s.93@gmail.com>
2020-04-12 16:25:21 +0900

    テスト実行完了したテストモデルslxファイル名を書き出せるようにした

--- run_all_test_signalbuilder/run_all_test.m
+++ run_all_test_signalbuilder/run_all_test.m
@@ -1,10 +1,13 @@
 clear all;
 
+msg_create_report = 'すべてのテストケースに合格したテストモデルslxファイル名を出力します。';
+
 %% 全テストモデルのファイルパスを取得する
 %% テストモデルslxが記載されたcsvファイルを読み込む
 filenames = readtable('filename_test_slx.csv','ReadVariableNames',true, 'Delimiter', ',');
 %% i個目のテストモデルを実行
 num_slx = length(filenames.Variables);
+pass = 1;
 for i=1:1:num_slx
     filename_cell = filenames(i,'filename').Variables;
     filename = filename_cell{1};
@@ -28,10 +31,25 @@ for i=1:1:num_slx
     num_group = length(groupnames);
     
     %% Group1からGroup num_groupまでシミュレーション実行する
-    
-    for j=1:1:num_group
-        signalbuilder(path_signal_builder, 'activegroup',j);
-        sim(filename);
+    %% シミュレーション実行
+    try
+        for j=1:1:num_group
+            signalbuilder(path_signal_builder, 'activegroup',j);
+            sim(filename); 
+        end   
+        %% テスト合格ファイル名を追加
+            filename_pass{pass} = filename;
+            pass = pass+1;
+    catch ME
+        % テスト合格ファイル名への追加をしない
+        % assertionエラーメッセージの表示
+        display(ME.message)
     end
-    
 end
+display(msg_create_report)
+
+%% テストレポート生成
+report_contents_header = {'filename'};
+report_contents_filename = filename_pass;
+report_contents = {report_contents_header{1},report_contents_filename{1:end}}'
+writecell(report_contents,'test_result.csv');
